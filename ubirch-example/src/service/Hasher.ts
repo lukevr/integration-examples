0a4fb6cd0514783c2c65b94ed2e860cbde72745b
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-07-10 13:57:24 +0300

    better hash

--- ubirch-example/src/service/Hasher.ts
+++ ubirch-example/src/service/Hasher.ts
@@ -6,7 +6,7 @@ export class Hasher {
 
     public makeHash(fields: Array<{name: string, value: string }>): string {
        fields.sort((a,b) => a.name.localeCompare(b.name) );
-       const oneString = fields.join("");
+       const oneString = fields.map(x => x.value).join("");
        const hash = crypto.createHash('sha3-256').update(oneString).digest('base64');
        return hash;
     }
